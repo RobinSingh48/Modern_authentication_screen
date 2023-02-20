a25c44f664b89bea8d4385b82ff6649d93277224
RobinSingh48 <robinsinghkaithal@gmail.com>
2023-02-20 15:48:51 +0530

    small fixes

--- lib/services/google_signin.dart
+++ lib/services/google_signin.dart
@@ -1,4 +1,6 @@
 
+
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -6,32 +8,26 @@ import 'package:google_sign_in/google_sign_in.dart';
 import '../pages/home_screen.dart';
 
 class GoogleSignInService {
-
-  signInWithGoogle(context) async {
+  Future<UserCredential> signInWithGoogle(context) async {
     // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    if(googleUser!=null){
-      try{
-        final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-        final credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth.accessToken,
-          idToken: googleAuth.idToken,
-        );
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
-      }on FirebaseAuthException catch(ex){
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(ex.toString().toUpperCase()), backgroundColor: Colors.red,duration: Duration(seconds: 3),
-          dismissDirection: DismissDirection.vertical,elevation: 0,));
-      }
-    }
-
     // Obtain the auth details from the request
-
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
 
     // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    if(googleUser!=null){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(),));
+
+    }
 
     // Once signed in, return the UserCredential
-
-
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
+
